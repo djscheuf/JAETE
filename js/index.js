351f40e354abd9e60a7fb1219fedf1af5e0a6dff
Daniel Scheufler <djscheufler@gmail.com>
2017-05-14 14:23:20 -0500

    Add User Input areas
    + Updated index for text area, FileName, and Password inputs
    + Added buttons for Load and Save 
    =n Click functions are merely placeholders
    + Updated CSS with new styles for text area.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 let $ = require('jquery')
-let count =0
 
 console.log('jquery loaded.')
-console.log('count initialized.')
 
-$('#clickCounter').text(count)
-$('#btn_Count').on('click', ()=>{
-  count++
-  $('#clickCounter').text(count)
-  console.log('clickCounter increased: ')
-  console.log(count);
+$('#btn_Load').on('click',()=>{
+  fn = $('#txt_FileName').val();
+  console.log('Load File: '+fn);
+  // if Fn is empty: Open File Browser Dialog
+  // else, attempt to open 'fn'.
+  // Load Contents into txt_Contents;
 })
+
+$('#btn_Save').on('click',()=>{
+  cnts = $('#txt_Content').val();
+  console.log('Save File: '+cnts);
+  // if Fn is empty: prompt for fileName
+  // else, attempt to save contents to 'fn';
+})
+// Eventually we will need to add functionality
+//  to use Password, and Crypto.
