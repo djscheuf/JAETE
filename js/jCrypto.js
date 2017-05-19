bd9fc0b607204ce8f26689e54274a0a3b055ece4
Daniel Scheufler <djscheufler@gmail.com>
2017-05-18 21:00:35 -0500

    Update Package.json
    + Added initial tests for jCrypto
    + many updates to package Json to fill in missing items
    =n should no list depencendies
    =n should now also direct to Repo properly.
    
    + Minor update to jCrypto, now using utf8

--- js/jCrypto.js
+++ js/jCrypto.js
@@ -17,55 +17,55 @@ jCrypto.prototype.ConfirmCryptoEnabled()
 jCrypto.prototype.encrypt(contents, password)
 {
 	// Pseudo PGP Scheme
-	
+
 	// hash+salt password
 	var passwordCipher = crypto.createCipher('sha256','Is it Secret? Is it Safe?');
-	var sPassword = contentCipher.update(password,'utf16','base64');
+	var sPassword = contentCipher.update(password,'utf8','base64');
 	sPassword += contentCipher.final('base64');
-	
+
 	// generate random key
 	var rKey = crypto.randomBytes(256).toString('base64');
-	
+
 	// encrypt contents with random key.
 	var contentCipher = crypto.createCipher('sha256',rKey);
-	var eContents = contentCipher.update(contents,'utf16','base64');
+	var eContents = contentCipher.update(contents,'utf8','base64');
 	eContents += contentCipher.final('base64');
-	
+
 	// encrypt random key with saltPassword.
 	var keyCipher = crypto.createCipher('sha256',sPassword);
-	var eKey = keyCipher.update(rKey,'utf16','base64');
+	var eKey = keyCipher.update(rKey,'utf8','base64');
 	eKey += keyCipher.final('base64');
 	
 	// combine eKey and eContents;
 	var output = eKey + '_JAETE_'+eContents;
-	
+
 	return output;
 }
 
 jCrypto.prototype.decrypt(contents, password)
 {
 	// Pseudo PGP Scheme
-	
+
 	// hash+salt password
 	var passwordCipher = crypto.createCipher('sha256','Is it Secret? Is it Safe?');
-	var sPassword = contentCipher.update(password,'utf16','base64');
+	var sPassword = contentCipher.update(password,'utf8','base64');
 	sPassword += contentCipher.final('base64');
-	
+
 	// split eKey and eContents
 	var arr = contents.split('_JAETE_');
 	var eKey = arr[0];
 	var eContents = arr[1];
-	
+
 	// decrypt eKey with saltPassword.
 	var keyDecipher = crypto.createDecipher('sha256',sPassword);
-	var rKey = keyDecipher.update(eKey,'base64','utf16');
-	rKey += keyDecipher.final('utf16');
-	
+	var rKey = keyDecipher.update(eKey,'base64','utf8');
+	rKey += keyDecipher.final('utf8');
+
 	// decrypt eContents with dKey.
 	var contentDecipher = crypto.createDecipher('sha256',rKey);
-	var contents = contentDecipher.update(eContents,'base64','utf16');
-	contents += contentDecipher.final('utf16');
-		
+	var contents = contentDecipher.update(eContents,'base64','utf8');
+	contents += contentDecipher.final('utf8');
+
 	return contents;
 }
 
